2ea3299545cec9787c34ae94228a0ee9af1d65e2
mohammed_fedora <mohammad.aljanabi@gmail.com>
2026-03-22 21:09:58 +0300

    refined ,now a check box that keep you on top so it wont disepar after clicking on other document or u uncheck it to behave normally ;;;dont work on wayland

--- central_widget.py
+++ central_widget.py
@@ -15,9 +15,9 @@ class central_widget(QWidget):
         self.setLayout(self.layout)
 
 
-        # self.toggle_textbox_checkbox = QCheckBox("Toggle Text Box")
-        # self.toggle_textbox_checkbox.setChecked(False)
-        # self.toggle_textbox_checkbox.stateChanged.connect(self.show_hide_textbox)
+        self.toggle_textbox_checkbox = QCheckBox("Toggle ontop")
+        self.toggle_textbox_checkbox.setChecked(False)
+        self.toggle_textbox_checkbox.stateChanged.connect(self.toggle_on_top)
 
         self.label_credit = QLabel(self)
         self.label_credit.setText("M.Soft Mohammed Sami")
@@ -31,33 +31,13 @@ class central_widget(QWidget):
 
         self.layout.addWidget(self.quick_refine_button, 0, 0)
         self.layout.addWidget(self.label_credit, 1, 0)
+        self.layout.addWidget(self.toggle_textbox_checkbox, 2, 0)
 
         self.setWindowIcon(QIcon("PDF2TXT.png"))
-        # self.layout.addWidget(self.toggle_textbox_checkbox, 0, 0)
-        self.setWindowFlags(
-            Qt.Window |
-            Qt.CustomizeWindowHint |
-            Qt.WindowMinimizeButtonHint |
-            Qt.WindowCloseButtonHint |
-            Qt.WindowStaysOnTopHint
-        )
-        self.setFixedSize(250, 70)
+
+
+        self.setFixedSize(250, 130)
         self.move_to_bottom_right()
-        # def refine(self):
-    #     if self.paste_text_edit.toPlainText() != "":
-    #         refined=[]
-    #         for text in self.paste_text_edit.toPlainText().split("\n"):
-    #             if text.endswith("\n") :
-    #                 refined.append(text[:-1])
-    #             else :
-    #                 refined.append(text)
-    #         self.paste_text_edit.blockSignals(True)
-    #         self.paste_text_edit.clear()
-    #         self.paste_text_edit.setPlainText(" ".join(refined))
-    #         self.paste_text_edit.blockSignals(False)
-    #         self.clipboard.setText(self.paste_text_edit.toPlainText())
-    #     else :
-    #         return
 
     def quick_refine(self):
         if self.clipboard.text() != "":
@@ -77,32 +57,25 @@ class central_widget(QWidget):
         y = screen.y() + screen.height() - self.height() - margin
 
         self.move(x, y)
-    # def show_hide_textbox(self):
-    #     if self.toggle_textbox_checkbox.isChecked():
-    #         self.paste_label = QLabel()
-    #         self.paste_label.setText("Paste here")
-    #         self.paste_label.setAlignment(Qt.AlignCenter)
-    #         self.paste_label.setStyleSheet("font-size:14px;font-weight:bold;font-family:Ubuntu;")
-    #
-    #         self.paste_text_edit=QTextEdit()
-    #         self.paste_text_edit.textChanged.connect(self.refine)
-    #         self.layout.addWidget(self.paste_label,1,0)
-    #         self.layout.addWidget(self.paste_text_edit,2,0)
-    #         self.setMinimumSize(600,250)
-    #         self.setMaximumSize(700, 400)
-    #
-    #     elif not self.toggle_textbox_checkbox.isChecked():
-    #         if hasattr(self, "paste_text_edit"):
-    #             self.layout.removeWidget(self.paste_text_edit)
-    #             self.paste_text_edit.setParent(None)
-    #             self.paste_text_edit.deleteLater()
-    #         if hasattr(self, "paste_label"):
-    #             self.layout.removeWidget(self.paste_label)
-    #             self.paste_label.setParent(None)
-    #             self.paste_label.deleteLater()
-    #             self.setMinimumSize(140, 71)
-    #             self.setMaximumSize(140,71)
+    def toggle_on_top(self):
+        if self.toggle_textbox_checkbox.isChecked():
+            self.setWindowFlags(
+            Qt.Window |
+            Qt.CustomizeWindowHint |
+            Qt.WindowMinimizeButtonHint |
+            Qt.WindowCloseButtonHint |
+            Qt.WindowStaysOnTopHint
+        )
+        else:
+            self.setWindowFlags(
+                Qt.Window |
+                Qt.CustomizeWindowHint |
+                Qt.WindowMinimizeButtonHint |
+                Qt.WindowCloseButtonHint
 
+            )
+        self.show()
+        self.raise_()
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = central_widget()
